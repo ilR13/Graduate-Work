e60e689347e6a5afc69c9e2358775da19bf08d38
ilR13 <qazcom203@gmail.com>
2024-06-11 09:56:54 +0300

    добавил поле для алгоритма

--- game.py
+++ game.py
@@ -3,13 +3,14 @@ from pygame.locals import QUIT, USEREVENT, MOUSEBUTTONDOWN
 from pygame.event import Event, post
 from random import randint
 import time
-gamewin = (850, 700)
+gamewin = (1300, 700)
 
 class Area():
-  def __init__(self, mw, x=0, y=0, width=10, height=10, color=None):
+  def __init__(self, mw, x, y, width, height, color, function):
       self.rect = pygame.Rect(x, y, width, height) #прямоугольник
       self.fill_color = color
       self.mw = mw
+      self.function = function
   def color(self, new_color):
       self.fill_color = new_color
   def fill(self):
@@ -18,6 +19,9 @@ class Area():
       pygame.draw.rect(self.mw, frame_color, self.rect, thickness)
   def collidepoint(self, x, y):
       return self.rect.collidepoint(x, y)
+
+  def colliderect(self, rect):
+      return self.rect.colliderect(rect)
 '''класс надпись'''
 class Label(Area):
   def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
@@ -35,17 +39,19 @@ class Game():
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((gamewin[0], gamewin[1]))
         self.background = pygame.transform.scale(pygame.image.load("сетка.png"), (700, 700))
+
         self.background_left = self.rect = pygame.Rect(0, 0, 150, gamewin[1])
+        self.background_middle = self.rect = pygame.Rect(150, 0, 500, gamewin[1])
         self.player = pygame.transform.scale(pygame.image.load("право.png"), (50, 50))
         self.direction = 90
-        self.button_forward = Label(self.screen,0,0,100,50,(255,0,0))
+        self.button_forward = Label(self.screen,0,0,100,50,(255,0,0),self.forward)
         self.button_forward.set_text("forward")
-        self.button_right = Label(self.screen,0,55,100,50,(255,0,0))
+        self.button_right = Label(self.screen,0,55,100,50,(255,0,0),self.rotate)
         self.button_right.set_text("right")
-        self.button_left = Label(self.screen,0,110,100,50,(255,0,0))
+        self.button_left = Label(self.screen,0,110,100,50,(255,0,0),self.rotate)
         self.button_left.set_text("left")
         self.rect = self.player.get_rect()
-        self.rect.x = 150
+        self.rect.x = 650
         self.rect.y = 0
         self.moving = False
 
@@ -58,12 +64,13 @@ class Game():
                 x, y = event.pos
                 if self.button_forward.collidepoint(x, y):
                     #self.forward()
-                    self.moving = True
+                    self.button_forward.function()
+                    #self.moving = True
                     #del self.button_forward
                 elif self.button_right.collidepoint(x, y):
-                    self.rotate(90)
+                    self.button_right.function(90)
                 elif self.button_left.collidepoint(x, y):
-                    self.rotate(-90)
+                    self.button_left.function(-90)
 
             if event.type == pygame.MOUSEMOTION:
                 if self.moving:
@@ -72,10 +79,14 @@ class Game():
                     self.button_forward.rect.y = self.button_forward.rect.y + y_new
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                 self.moving = False
+                if not(self.button_forward.colliderect(self.background_middle)):
+                    self.button_forward.rect.x = 0
+                    self.button_forward.rect.y = 0
         self.update()
 
     def update(self):
-        self.screen.blit(self.background, (150, 0))
+        self.screen.blit(self.background, (650, 0))
+        pygame.draw.rect(self.screen,(0,255,0),self.background_middle)
         pygame.draw.rect(self.screen, (0,0,255), self.background_left)
         self.screen.blit(self.player, (self.rect.x, self.rect.y))
         self.button_forward.draw(30, 15)
